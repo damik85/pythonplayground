c599fa06ecbc65d09e2abf6e423ebb8da95889e9
damik85 <81721290+damik85@users.noreply.github.com>
2021-04-08 17:24:48 -0300

    Update dolar_calculator.py

--- dolar_calculator.py
+++ dolar_calculator.py
@@ -2,6 +2,7 @@ import requests
 import json
 import sys
 
+
 ###############
 ## FUNCTIONS ##
 ###############
@@ -10,7 +11,8 @@ def divide(x, y):
     if y > 0:
         return x / y
     else:
-         return "not able to divide 0"
+        return "not able to divide 0"
+
 
 def menu():
     print("Calculadora de cotizacion del dolar del dia.")
@@ -20,30 +22,28 @@ def menu():
     print("Cualquier otro numero: Exit")
     print("------------------------------------")
 
-url = 'http://api-dolar-argentina.herokuapp.com/api/dolaroficial'
-respuesta = requests.get(url)
-if respuesta.status_code == 200:
-    data = respuesta.json()
-    num2 = json.dumps(data['venta'], indent=4)
-    num2 = float(num2.replace('"', ''))
 
-url = 'http://api-dolar-argentina.herokuapp.com/api/dolarblue'
-respuesta = requests.get(url)
-if respuesta.status_code == 200:
-    data = respuesta.json()
-    num3 = json.dumps(data['venta'], indent=4)
-    num3 = float(num3.replace('"', ''))
+def get_dolar_value(url):
+    respuesta = requests.get(url)
+    if respuesta.status_code == 200:
+        data = respuesta.json()
+        dolar_value = json.dumps(data['venta'], indent=4)
+        dolar_value = float(dolar_value.replace('"', ''))
+    return dolar_value
+
 
 def add_input(msg):
     n = float(input(msg))
     return n
 
+
 def calculate_dolar(num1, num2, choice):
     if choice == 1:
         result = divide(num1, num2)
     if choice == 2:
-        result = divide(num1, num3)
+        result = divide(num1, num2)
     return result
+
 
 ###############
 ##   MAIN    ##
@@ -60,6 +60,15 @@ if __name__ == '__main__':
 
         if choice in valid_options:
             num1 = add_input("Ingresa los pesos a convertir: ")
+
+            if choice == 1:
+                url_dolar = 'http://api-dolar-argentina.herokuapp.com/api/dolaroficial'
+
+            if choice == 2:
+                url_dolar = 'http://api-dolar-argentina.herokuapp.com/api/dolarblue'
+
+            num2 = get_dolar_value(url_dolar)
+
             calculate_dolar_result = calculate_dolar(num1, num2, choice)
             calculate_dolar_result = round(calculate_dolar_result, 2)
         else:
